2ad1f410173f83cb10448e6abb287c3ba93a4725
derek-williams00 <derekOfficial00@gmail.com>
2023-05-20 12:19:59 -0700

    cleaned up main.py

--- ispyai-background-service/main.py
+++ ispyai-background-service/main.py
@@ -8,37 +8,27 @@ import tiktoken
 # Used to get transcripts for videos
 from transcripts import export_youtube_transcript
 
+# Initialize Flask app
+APP_NAME = "iSpyAI"
+DEBUG = True
+app = Flask(APP_NAME)
+
 # Load OpenAI API key from file
 with open("openai_api_key.txt", "r") as f:
     openai.api_key = f.read()
-
-# Initialize Flask app
-APP_NAME = "iSpyAI"
-app = Flask(APP_NAME)
-
-''' EXAMPLE REQUEST to OPENAI API:
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Hello! How are you?",
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"],
-    )
-'''
 
 # Limit on number of GPT3 tokens per request
 TOKEN_LIMIT = 2000
 
 @app.route("/api/generate", methods=["POST"])
 def generate():
-    print("Generating new blog for {} ...".format(request.json["url"]))
+    if DEBUG:
+        print("Generating new blog for {} ...".format(request.json["url"]))
     transcript = export_youtube_transcript(request.json["url"])
     tokenizer = tiktoken.Tokenizer(tokenization_standard="gpt3")
     token_count = len(tokenizer.tokenize(transcript))
-    print("-- Token count: {} / {}".format(token_count, TOKEN_LIMIT))
+    if DEBUG:
+        print("-- Token count: {} / {}".format(token_count, TOKEN_LIMIT))
     if token_count > TOKEN_LIMIT:
         return "Error: Transcript too long. Please use a shorter video.", 400
     prmt = "Please summarize the following transcript:\n\n" + transcript
@@ -57,4 +47,4 @@ def generate():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=DEBUG)
