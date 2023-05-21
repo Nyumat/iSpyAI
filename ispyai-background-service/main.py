76f4f7206290c4fa77bb6ae0cbc90bdbbc731df5
Toby Loki <tobyloki5@gmail.com>
2023-05-20 22:51:05 -0700

    (1) ispyai-background-service finished setting up main.py to save s3 file and save presignedUrl to redis (2) ispyai-rest-api added cors + added api path to get all data entries for userId + updated /submitJob path to save initial redis data entry

--- ispyai-background-service/main.py
+++ ispyai-background-service/main.py
@@ -4,14 +4,11 @@ import hashlib
 import boto3
 from botocore.exceptions import ClientError
 import logging
-
 import redis
 
 s3 = boto3.client('s3', region_name='us-west-2', config=boto3.session.Config(signature_version='s3v4'))
 
-
 BUCKET_NAME = "video-to-blog"
-
 
 def create_presigned_url(key_object_name, expiration=3600):
     """Generate a presigned URL to share an S3 object
@@ -34,41 +31,25 @@ def create_presigned_url(key_object_name, expiration=3600):
     # The response contains the presigned URL
     return response
 
-
-def main():
-    print("Starting job...")
-
-    # print env variables
-    userId = os.environ.get('userId')
-    videoUrl = os.environ.get('videoUrl')
-    # userId = "testUserId"
-    # videoUrl = "testVideoUrl"
-
-    print(f"env variable userId: {userId}")
-    print(f"env variable videoUrl: {videoUrl}")
-
-    # assert that userId and videoUrl are not None and not empty after being trimmed
-    assert userId is not None and userId.strip() != ""
-    assert videoUrl is not None and videoUrl.strip() != ""
-
+def saveBlogToS3(videoUrlHash, content):
     # create hash of videoUrl
-    hash = hashlib.sha256(videoUrl.encode('utf-8')).hexdigest()
+    # hash = hashlib.sha256(videoUrl.encode('utf-8')).hexdigest()
 
-    key = hash
+    s3ObjectKey = f"{videoUrlHash}.md"
 
-    print(f"key: {key}")
+    print(f"s3ObjectKey: {s3ObjectKey}")
 
     # create file called {key} and save to /tmp/{key}
-    file_path = f"/tmp/{key}"
+    file_path = f"/tmp/{s3ObjectKey}"
     with open(file_path, "w") as file:
-        file.write("First line of text\nSecond line of text\n")
+        file.write(content)
 
     # Upload the file to the s3 bucket
     # s3.upload_file(file_path, Bucket=BUCKET_NAME, Key=key)
     expiration_time = datetime.datetime.now() + datetime.timedelta(hours=1)
     s3.put_object(
         Bucket=BUCKET_NAME,
-        Key=key,
+        Key=s3ObjectKey,
         Expires=expiration_time,
         Body=open(file_path, 'rb')
     )
@@ -77,11 +58,13 @@ def main():
     os.remove(file_path)
 
     # Generate a presigned url for the file in the s3 bucket
-    presigned_url = create_presigned_url(key)
+    presigned_url = create_presigned_url(s3ObjectKey)
 
     print(f"presigned url: {presigned_url}")
 
+    return presigned_url
 
+def savePresignedUrlToRedis(userId, videoUrlHash, presigned_url):
     # add element to redis
     r = redis.Redis(
         host='redis-18674.c273.us-east-1-2.ec2.cloud.redislabs.com',
@@ -89,21 +72,49 @@ def main():
         username='default',
         password='MNSwDR1UOlq20ucYUhfoaoW5OSHIuEnY')
     
-    redisKey = f"{userId} - {key}"
+    redisKey = f"{userId}-{videoUrlHash}"
 
-    r.hset(redisKey, mapping={
-        'videoUrl': videoUrl,
-        "presignedUrl": presigned_url
-    })
+    # get the value of the key
+    r.hset(redisKey, "presignedUrl", presigned_url)
     r.expire(redisKey, 3600)
 
     # disconnect from redis
     r.close()
 
+def main():
+    print("Starting job...")
 
-# Import flask app from server.py
-from server import app
+    # print env variables
+    userId = os.environ.get('userId')
+    videoUrl = os.environ.get('videoUrl')
+    videoUrlHash = os.environ.get('videoUrlHash')
+    # userId = "testUserId"
+    # videoUrl = "testVideoUrl"
+    # videoUrlHash = "testVideoUrlHash"
+
+    print(f"env variable userId: {userId}")
+    print(f"env variable videoUrl: {videoUrl}")
+    print(f"env variable videoUrlHash: {videoUrlHash}")
+
+    # assert that env variables are not None and not empty after being trimmed
+    assert userId is not None and userId.strip() != ""
+    assert videoUrl is not None and videoUrl.strip() != ""
+    assert videoUrlHash is not None and videoUrlHash.strip() != ""
+
+    # TODO: 1. get video transcript from videoUrl
+
+
+    # TODO: 2. generate blog post from video transcript and save to content
+    content = "This is a test blog post. It is very short.\nHello there yo!"
+
+    # save blog to s3
+    presigned_url = saveBlogToS3(videoUrlHash, content)
+
+    # save presigned url to redis
+    savePresignedUrlToRedis(userId, videoUrlHash, presigned_url)
+
+    print("Job complete!")
+    
 
 if __name__ == "__main__":
     main()
-    app.run(debug=True)
---
+++ ispyai-rest-api/ispyai-rest-api/Models/JobEntryModel.cs
@@ -0,0 +1,10 @@
+using Newtonsoft.Json.Linq;
+using StackExchange.Redis;
+
+namespace ispyai_rest_api.Models;
+
+public class JobEntry
+{
+    public string key { get; set; }
+    public string value { get; set; }
+}
